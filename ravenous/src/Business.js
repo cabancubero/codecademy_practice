379078056d0aafab611d674314b8d1106d6d34ea
cabancubero <162509767+cabancubero@users.noreply.github.com>
2025-03-02 21:33:25 -0600

    create buisness component

---
+++ ravenous/src/Business.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Business(){
+    return (
+        <div className = "business">
+            <img src='' alt=''/>
+            <h2>Business Name</h2>
+            <p>Address</p>
+            <p>City</p>
+            <p>State, Zip</p>
+            <p>Category</p>
+            <p>Rating</p>
+            <p>review count</p>
+        </div>
+    );
+}
