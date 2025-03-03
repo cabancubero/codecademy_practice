77c1ba4321148fff58274d2815d8f60caa447164
cabancubero <162509767+cabancubero@users.noreply.github.com>
2025-03-02 21:36:05 -0600

    added business object

--- ravenous/src/Business.js
+++ ravenous/src/Business.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+const businessObject = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+};
+
 function Business(){
     return (
         <div className = "business">
